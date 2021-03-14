9b6c4a9e825f5fe26f7e6259ad1bfb319c20f99d
Khaled-Mosharrof <khaleddiganto@mail.com>
2021-03-14 11:19:33 +0600

    third commit

--- MID_LAB_EXAM/Registration.php
+++ MID_LAB_EXAM/Registration.php
@@ -7,4 +7,19 @@
         if(empty($_POST['id']) || empty($_POST['password']) || empty($_POST['name']) || empty($_POST['usertype']) ){
             echo"Plaese fill out all the field";
         }
+        else{
+            //validation check
+
+            if ($temp_id[0]>0) {
+                echo"Please select year before 2015.";
+            }
+            else{
+
+                $id= $_POST['id'];
+                $password=$_POST['password'];
+                $repass=$_POST['repass'];
+                $name = $_POST['name'];
+                $type=$_POST['type'];
+
+                $user=['id'=> $id, 'password'=> $password, 'name' => $name, 'type' => $type ];
         ?>
